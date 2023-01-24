b6c4fd46c9249a12f1c15ffa3da86b675c263fcd
TakayaMiyachi <https://github.com/TakayaMiyachi/studyHTML.git>
2023-01-24 21:01:56 +0900

    関数のメモ化

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+import { useCallback } from "react";
 import { useState, memo } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
+
 export const App = memo(() => {
   console.log("Appレンダリング");
-
+  
   const [num, setNum] = useState(0);
-
+  
   const onClickButton = () => {
     setNum((oldNum) => oldNum + 1);
   };
 
+  const onClickReset =  useCallback(() => {
+    setNum(0);
+  },[]);
+  
   return (
     <>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
-      <Child1 />
+      {/*↓Propsとして関数を設定*/}
+      <Child1 onClickReset={onClickReset} />
       <Child4 />
     </>
   );
